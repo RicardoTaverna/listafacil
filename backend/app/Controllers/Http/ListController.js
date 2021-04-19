3581b3be9726d3154d2da89456f3629517c912c3
Ricardo Taverna <taverna.ricardo@gmail.com>
2021-04-19 20:18:17 -0300

    [LF-12] - Adicionado Docstrings e ESLint

--- backend/app/Controllers/Http/ListController.js
+++ backend/app/Controllers/Http/ListController.js
@@ -2,22 +2,46 @@
 
 const List = use("App/Models/List");
 class ListController {
-
+  /**
+	 * Show a list of all lists.
+	 * GET lists
+	 *
+	 * @param {object} ctx
+   * @param {auth} ctx.auth
+	 * @param {Request} ctx.request
+	 * @param {Response} ctx.response
+	 */
   async index ({params, auth, request, response}) {
     const list = await List.query().where('user_id', auth.user.id).fetch()
 
     return list
   }
 
+  /**
+	 * Show a list of all lists.
+	 * GET lists
+	 *
+	 * @param {object} ctx
+   * @param {auth} ctx.auth
+	 * @param {Response} ctx.response
+	 */
   async show ({ params, auth, response }) {
     const list = await List.findOrFail(params.id)
     if (list.user_id !== auth.user.id) {
       return response.status(401).send({ error: 'Not authorized' })
     }
   
-  
     return list
   }
+
+  /**
+	 * Create/save a new lists.
+	 * POST lists
+	 *
+	 * @param {auth} ctx.auth
+	 * @param {Request} ctx.request
+	 * @param {Response} ctx.response
+	 */
   async store ({ auth, request, response }) {
     const { id } = auth.user
     const data = request.only([
@@ -33,7 +57,15 @@ class ListController {
   async edit ({ params, request, response, view }) {
   }
 
-
+  /**
+	 * Update lists details.
+	 * PUT or PATCH lists/:id
+	 *
+	 * @param {object} ctx
+	 * @param {Request} ctx.request
+   * @param {auth} ctx.auth
+	 * @param {Response} ctx.response
+	 */
   async update ({ params, auth, request, response }) {
     const list = await List.findOrFail(params.id);
     if (list.user_id !== auth.user.id) {
@@ -50,6 +82,14 @@ class ListController {
     return list;
   }
 
+  /**
+	 * Delete a lists with id.
+	 * DELETE lists/:id
+	 *
+	 * @param {object} ctx
+	 * @param {auth} ctx.auth
+	 * @param {Response} ctx.response
+	 */
   async destroy ({ params, auth, response }) {
     const list = await List.findOrFail(params.id)
   
--- backend/app/Controllers/Http/UserController.js
+++ backend/app/Controllers/Http/UserController.js
@@ -3,23 +3,46 @@
 const User = use("App/Models/User");
 
 class UserController {
-    // Classe da controller do usuário.
+    /**
+	 * Create/save a new user.
+	 * POST user
+	 *
+	 * @param {Request} ctx.request
+	 */
     async create({ request }){
         const data = request.only(["username", "email", "password"]);
         const user = await User.create(data)
         return user;
     }
 
+    /**
+	 * Show a list of all user.
+	 * GET user
+	 *
+	 */
     async list_all(){
         const users = await User.all();
         return users;
     }
 
+    /**
+	 * Show a list of all user.
+	 * GET user
+	 *
+	 * @param {object} ctx
+	 */
     async list_by_id({ params }){
         const user = await User.findOrFail(params.id);
         return user;
     }
 
+    /**
+	 * Update product details.
+	 * PUT or PATCH user/:id
+	 *
+	 * @param {Request} ctx.request
+	 * @param {Response} ctx.response
+	 */
     async update({ params, request }){
         const user = await User.findOrFail(params.id);
         const data = request.only(["username", "email"]);
@@ -30,6 +53,12 @@ class UserController {
         return user;
     }
 
+    /**
+	 * Delete a user with id.
+	 * DELETE user/:id
+	 *
+	 * @param {object} ctx
+	 */
     async delete({ params }){
         const user = await User.findOrFail(params.id);
         await user.delete();
