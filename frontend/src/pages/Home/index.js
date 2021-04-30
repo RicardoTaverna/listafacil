4bc0a2932d128f0fc4498646bc5a42261f5920a8
Ricardo Taverna <taverna.ricardo@gmail.com>
2021-04-30 00:46:30 -0300

    [LF-18] - pagina de login

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,25 +1,43 @@
 import React from 'react';
 import './home.css';
-import logo from './ListaFácil_removebg.png';
+import logo from '../../images/ListaFácil_removebg.png';
 import { Button } from 'primereact/button';
 
 class Home extends React.Component {
     render() {       
         return (
-
-            <div>
-                <h1 style={{'padding-top': '60px', 'text-align':'center'}}>Bem vindo ao</h1>
-                    
-                <img src={logo} alt="listafácil" style={{'display':'block', 'margin-left': 'auto', 'margin-right': 'auto', 'padding-top':'60px' }}></img>
-                <h3 style={{'padding-top': '60px', 'text-align':'center'}}>
-                    Crie agora sua conta e faça sua primeira lista de compras inteligente!
-                </h3>
-                <div style={{'padding-top':'60px', 'text-align':'center' }}>
-                    <Button label="Criar Conta" style={{'margin-right':'0.5rem'}} />
-                    <Button label="Login" className="p-button-outlined" />
+        <React.Fragment>
+            <div class="topnav">
+                <div class="align-right" style={{'margin-right':'2rem'}}>
+                    <a class="active" href="/login">Login</a>
+                    <a href="https://github.com/RicardoTaverna/listafacil/issues">Problemas</a>
+                    <a href="https://github.com/RicardoTaverna/listafacil#readme">Sobre</a>
                 </div>
             </div>
-            
+
+            <header>
+                <div>
+                    <h1 className="mt-3 text-center">Bem vindo ao</h1>
+                        
+                    <img src={logo} alt="listafácil" className="header-image"></img>
+                    <h3 className="mt-3 text-center">
+                        Crie agora sua conta e faça sua primeira lista de compras inteligente!
+                    </h3>
+                    <div className="mt-3 text-center">
+                        <Button label="Criar Conta" className="p-button-primary" style={{'margin-right':'1rem'}} />
+                        <Button label="Login" className="p-button-primary p-button-outlined" />
+                    </div>
+                </div>
+            </header>
+
+            <div className="footer">
+                <p className="footer-text">CRIADO POR RICARDO TAVERNA & YGOR STENGRAT</p>
+                <Button className="p-button-primary p-button-text">
+                    <i className="pi pi-github p-px-3"></i>
+                    <span className="p-px-3"> Código Fonte</span>
+                </Button>
+            </div>
+        </React.Fragment>
         )
     }
 }
--- frontend/src/services/routes.js
+++ frontend/src/services/routes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { isAuthenticated } from './../services/auth';
 import Home from './../pages/Home/index'
+import Login from './../components/Login/index';
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
     <Route
@@ -20,7 +21,7 @@ const Routes = () => (
         <Switch>
             <Route exact path="/" component={Home} />
             <Route path="/cadastro" component={() => <h1>Cadastro</h1>} />
-            <Route path="/login" component={() => <h1>Login</h1>} />
+            <Route path="/login" component={Login} />
             <PrivateRoute path="/app" component={() => <h1>App</h1>} />
             <Route path="*" component={() => <h1>Page not found</h1>} />
         </Switch>
